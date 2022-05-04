a6c8d317bc48ca15b7007715ce8d4b293cebfb60
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2022-05-04 18:45:53 +0300

    somehow it dosen't work when combined & deleted unnecessary lines from the script

--- kind-setup.sh
+++ kind-setup.sh
@@ -2,11 +2,3 @@
 
 kind create cluster --config ansible/required-files/kind-cluster.yaml
 kubectl config view --flatten > ansible/required-files/kind-config.yaml
-
-cat << EOF > ansible/required-files/cluster-info.yaml
----
-masterNode: kind-control-plane
-workerNode1: kind-worker
-workerNode2: kind-worker2
-kubeconfig: ~/.kube/kind-config
-EOF
