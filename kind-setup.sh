518b55a85f387f724f924ec0932502a19a18b3ae
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2022-05-05 17:31:51 +0300

    script test 1

--- kind-setup.sh
+++ kind-setup.sh
@@ -2,3 +2,11 @@
 
 kind create cluster --config ansible/required-files/kind-cluster.yaml
 kubectl config view --flatten > ansible/required-files/kind-config.yaml
+
+docker build -t vaskatevas/jenkins -f Dockerfile .
+docker run --network host  vaskatevas/jenkins ansible-playbook openwhisk-function/ansible/cluster-setup.yml
+#docker run --network host  vaskatevas/jenkins ansible-playbook openwhisk-function/ansible/openwhisk-setup.yml
+
+# Note 
+# remove wskdeploy from openwhisk-setup.yml
+# and add an loop that deploys untill its successful
