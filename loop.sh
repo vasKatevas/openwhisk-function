e8a311d5e2c273791c0409e7308f09355b2601ed
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2022-05-06 21:43:03 +0300

    corrected some paths

--- loop.sh
+++ loop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-while [ -z "$(wskdeploy -m /openwhisk-function/manifest.yaml 2>/dev/null )" ]; do sleep 3m; done
+while [ -z "$(wskdeploy -m ../manifest.yaml 2>/dev/null )" ]; do sleep 3m; done
