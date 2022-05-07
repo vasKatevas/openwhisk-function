f4380d6938bde8a5eec1103d6b93b94c76c93d83
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2022-05-07 16:07:57 +0300

    starting to change ow configs

--- kind-setup.sh
+++ kind-setup.sh
@@ -5,7 +5,7 @@ kubectl config view --flatten > ansible/required-files/kind-config.yaml
 
 docker build -t vaskatevas/jenkins -f Dockerfile .
 docker run --network host  vaskatevas/jenkins ansible-playbook openwhisk-function/ansible/cluster-setup.yml
-docker run --network host  vaskatevas/jenkins /bin/bash -c  "ansible-playbook openwhisk-function/ansible/openwhisk-setup.yml; ./openwhisk-function/loop.sh"
+docker run --network host  vaskatevas/jenkins /bin/bash -c  "ansible-playbook openwhisk-function/ansible/openwhisk-setup.yml"
 # Note 
 # remove wskdeploy from openwhisk-setup.yml
 # and add an loop that deploys untill its successful
