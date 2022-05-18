a428094bc834a569d055238697a3cbe0d058dd51
vasKatevas <42468609+vasKatevas@users.noreply.github.com>
2022-05-18 20:39:19 +0300

    mycluster dynamic change & jmeter setup

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,11 @@ WORKDIR /home/user
 
 USER root
 # Install requirments apt & pip
-RUN apt-get update && apt-get install -y tar wget git curl python3-pip python3 iputils-ping
+RUN apt-get update && apt-get install -y tar wget git curl python3-pip python3 iputils-ping bc default-jdk
 RUN pip3 install ansible
 RUN pip3 install kubernetes
 RUN apt-get install -y vim
+RUN echo "JAVA_HOME=\"/usr/lib/jvm/java-11-openjdk-amd64\"" >> /etc/environment
 
 USER user
 ## ansible playbooks 
@@ -24,7 +25,7 @@ RUN wget https://github.com/apache/openwhisk-wskdeploy/releases/download/1.2.0/o
 RUN mv wskdeploy /usr/local/bin/
 RUN curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 RUN install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-RUN wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.tgz && tar -zxvf apache-jmeter-5.4.3.tgz && ln -s /apache-jmeter-5.4.3/bin/jmeter /usr/local/bin/
+RUN wget https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.tgz && tar -zxvf apache-jmeter-5.4.3.tgz && ln -sr apache-jmeter-5.4.3/bin/jmeter /usr/local/bin/
 
 # Copy kind-config ( every time kind runs it makes a new one ) 
 COPY ./ansible/required-files/kind-config.yaml /home/user/.kube/config
--- kind-setup.sh
+++ kind-setup.sh
@@ -4,8 +4,22 @@ kind create cluster --config ansible/required-files/kind-cluster.yaml
 kubectl config view --flatten > ansible/required-files/kind-config.yaml
 
 docker build -t vaskatevas/jenkins -f Dockerfile .
-docker run --network host  vaskatevas/jenkins ansible-playbook openwhisk-function/ansible/cluster-setup.yml
-docker run --network host  vaskatevas/jenkins /bin/bash -c  "ansible-playbook openwhisk-function/ansible/openwhisk-setup.yml"
-# Note 
-# remove wskdeploy from openwhisk-setup.yml
-# and add an loop that deploys untill its successful
+docker run -d  --network host  vaskatevas/jenkins ping 127.0.0.1
+containerID=$(docker ps | grep vaskatevas/jenkins | awk '{print $1}')
+docker exec $(echo $containerID) ansible-playbook openwhisk-function/ansible/cluster-setup.yml
+docker exec $(echo $containerID) ansible-playbook openwhisk-function/ansible/openwhisk-setup.yml
+docker exec $(echo $containerID) jmeter -n -t openwhisk-function/HTTP-Request.jmx
+
+#ansible-playbook edit-mycluster.yml --extra-vars "minMemory= maxMemory= stdMemory= userMemory="
+
+#Values
+#cont.Count         2      /    4    /   6
+#memory:
+#  min: "512m"             /
+#  max: "2048m"            /
+#  std: "1024m"            /
+#  containerPool:
+#      userMemory: "2048m" / 4096m  / 8192m
+
+# average Latency
+docker exec $(echo $containerID) /bin/bash -c "cd openwhisk-function ; echo \"\$(cat result.csv | paste -sd+ | bc) / \$(wc -l result.csv | awk '{ print \$1 }')\" | bc"
